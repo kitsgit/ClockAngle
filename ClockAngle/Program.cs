9549b5976230e77059656ba3a3b5bf5fd37e6c13
kitsgit <christina.aswez@gmail.com>
2020-02-20 08:33:05 +0530

    Add project files.

---
+++ ClockAngle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ente the hour : ");
+            float h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the minute : ");
+            float m = int.Parse(Console.ReadLine());
+            float angle = ((h / 12) * 360) - ((m / 60) * 360);
+            float h_offset = (360 / 12) / (60 / m);
+            angle = angle + h_offset;
+            if (angle > 180)
+                angle = 360-angle;
+            Console.WriteLine("The angle is : " + angle);
+            Console.ReadLine();
+        }
+    }
+}
